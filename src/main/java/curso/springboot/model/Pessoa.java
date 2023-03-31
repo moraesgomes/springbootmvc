e5532d2d70a7647721951752fcf08e8719c52e34
haroldo gomes <moraesgomes@gmail.com>
2023-03-30 21:51:02 -0300

    Classe de geração de Relatórios

--- src/main/java/curso/springboot/model/Pessoa.java
+++ src/main/java/curso/springboot/model/Pessoa.java
@@ -13,37 +13,94 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-
-
 @Entity
 public class Pessoa implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
-	@NotNull(message = "Nome não pode ser nulo")
-	@NotEmpty(message ="Nome não poder ser vazio" )
+
+	@NotNull(message="Nome não pode ser nulo")
+	@NotEmpty(message = "Nome não pode ser vazio")
 	private String nome;
-	
-	@NotNull(message = "Spbrenome não pode ser nulo")
+
+	@NotNull(message = "Sobrenome não pode ser nulo")
 	@NotEmpty(message = "Sobrenome não pode ser vazio")
 	private String sobrenome;
 	
-	@Min(value = 18 , message = "Idade inválida")
+	@Min(value = 18, message = "Idade inválida")
 	private int idade;
 	
-	@OneToMany(mappedBy = "pessoa", orphanRemoval =true , cascade = CascadeType.ALL )
-	private List<Telefone> fones;
+	private String cep;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	private String ibge;
 	
-	public void setFones(List<Telefone> fones) {
-		this.fones = fones;
+	private String sexopessoa;
+	
+	@OneToMany(mappedBy="pessoa", orphanRemoval= true, cascade = CascadeType.ALL)
+	private List<Telefone> telefones;
+	
+	
+	
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getIbge() {
+		return ibge;
+	}
+
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
+	}
+
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
 	}
 	
-	public List<Telefone> getFones() {
-		return fones;
+	public List<Telefone> getTelefones() {
+		return telefones;
 	}
 	
 	public void setIdade(int idade) {
@@ -77,8 +134,13 @@ public class Pessoa implements Serializable {
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	
+	public void setSexopessoa(String sexopessoa) {
+		this.sexopessoa = sexopessoa;
 	}
+	
+	public String getSexopessoa() {
+		return sexopessoa;
+	}
+
 }
--- src/main/java/curso/springboot/security/WebConfigSecurity.java
+++ src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -1,18 +1,25 @@
 package curso.springboot.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter  {
+	
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
 	
 	@Override // Configura as solicitações de acesso via HTTP
 	protected void configure(HttpSecurity http) throws Exception {
@@ -21,19 +28,21 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter  {
 		.disable() // Desativa as configurações padrão de memória do spring.
 		.authorizeRequests() //Permite restringir acessos
 		.antMatchers(HttpMethod.GET,"/").permitAll() //Qualquer usuário tem acesso a página incial
+		.antMatchers(HttpMethod.GET,"/cadastropessoa").hasAnyRole("ADMIN")
 		.anyRequest().authenticated()
 		.and().formLogin().permitAll() // Permite qualquer usuário acessar
-		.and().logout() // Mapeia URL de Logout e invalida usuário autenticado
+		.loginPage("/login")
+		.defaultSuccessUrl("/cadastropessoa")
+		.failureUrl("/login?error=true")
+		.and().logout().logoutSuccessUrl("/login") // Mapeia URL de Logout e invalida usuário autenticado
 		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 
 	 @Override // Cria autenticação do usuário com o banco de dados ou em memória
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		 
-		 auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance() )
-		 .withUser("haroldo")
-		 .password("jesus100")
-		 .roles("ADMIN");
+		 auth.userDetailsService(implementacaoUserDetailsService)
+		 .passwordEncoder(new BCryptPasswordEncoder());
 		 
 		 
 	}
@@ -42,7 +51,10 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter  {
 	public void configure(WebSecurity web) throws Exception {
 		 
 		 web.ignoring().antMatchers("/materialize/**");
+		
 	}
+	 
+	 
 	 
 	 
 
